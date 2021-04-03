af8838fbc56c1734e1ac0bbee57522ee1037a6db
platymelon <kamonohashi995@gmailcom>
2021-04-03 14:55:35 +0100

    Initial commit

---
+++ Assets/Scripts/CubeCollision.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //if we enter a collision and the object we collided with is tagged enemy
+        // destroy the game object we hit
+        if (collision.gameObject.tag == "enemy")
+            {
+            Destroy(collision.gameObject);
+            Debug.Log("Destroyed");
+        }
+    }
+}
---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    public float Speed;
+    public float Force;
+}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RenderBuffer.Speed = Vector2.up * ForceMode;    
+    }
+}
